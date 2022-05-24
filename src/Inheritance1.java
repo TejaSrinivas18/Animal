4e457d732b42d3bbe77f866198f1442283608ae6
TejaSrinivas18 <tejasrinivas19@gmail.com>
2022-05-24 23:05:12 +0530

    first commit

---
+++ src/Inheritance1.java
@@ -0,0 +1,32 @@
+class Animal{
+	void eat() {
+		System.out.println("eating");
+	}
+	void sleep() {
+		System.out.println("sleeping");
+	}
+}
+
+class Dog extends Animal {
+	void bark() {
+		System.out.println("barking");
+	}
+}
+
+class BabyDog extends Dog{
+	void weep() {
+		System.out.println("weeping");
+	}	
+}
+
+public class Inheritance1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BabyDog bd = new BabyDog();
+		bd.weep();
+		bd.bark();
+		bd.eat();
+		bd.sleep();
+	}
+}
